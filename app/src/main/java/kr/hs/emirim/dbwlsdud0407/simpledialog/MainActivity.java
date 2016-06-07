e30e9ba1d45cefc457b74e280c37056f755fd22d
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-06-07 19:52:48 +0900

    #2 Create alertDialog Buller

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/simpledialog/MainActivity.java
@@ -1,13 +1,33 @@
 package kr.hs.emirim.dbwlsdud0407.simpledialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but=(Button)findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("First Dialog");
+        dialog.setMessage("this is message part.");
+        dialog.setIcon(R.drawable.images);
+        dialog.setPositiveButton("확인",null);
+        dialog.show();
     }
 }
