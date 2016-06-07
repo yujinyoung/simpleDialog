5306ccb0a0e9697a089827a745193f03a16fdb2e
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-06-07 20:27:39 +0900

    #3 dialog.setPositiveButton Event

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/simpledialog/MainActivity.java
@@ -1,10 +1,13 @@
 package kr.hs.emirim.dbwlsdud0407.simpledialog;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,7 +30,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("First Dialog");
         dialog.setMessage("this is message part.");
         dialog.setIcon(R.drawable.images);
-        dialog.setPositiveButton("확인",null);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+             Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭하셨습니다.",Toast.LENGTH_LONG ).show();
+            }
+        });
         dialog.show();
     }
 }
